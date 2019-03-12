64f72d93494a270936cdabe91d50476ea22acd62
xiao.xinmiao <xiaoxm_001@163.com>
2019-03-13 06:06:58 +0800

    #2 just one compose

--- main.go
+++ main.go
@@ -28,9 +28,10 @@ type ConfigDto struct {
 	GopathDocker string
 }
 type ProjectDto struct {
-	Name  string
-	Envs  []string
-	Ports []string
+	Name     string
+	Envs     []string
+	Ports    []string
+	SubNames []string
 }
 
 func main() {
@@ -40,24 +41,31 @@ func main() {
 		fmt.Printf("read config error:%v", err)
 		return
 	}
-	if c.IsKafka {
-		if err = KafkaCompose(); err != nil {
-			fmt.Printf("write to kafka error:%v", err)
-			return
-		}
+	viper := viper.New()
+	setComposeMysql(viper, c.Mysql.Sqlpath, c.Mysql.Envs)
+	setComposeKafka(viper)
+
+	for _, projectDto := range c.SubProjects {
+		c.Project.SubNames = append(c.Project.SubNames, projectDto.Name)
 	}
-	if c.Mysql.IsUpdate {
-		if err = MysqlCompose(c.Mysql.Sqlpath, c.Mysql.Envs); err != nil {
-			fmt.Printf("write to mysql error:%v", err)
-			return
-		}
-	}
-	if err = AppCompose(c.Gopath, c.Project, c.SubProjects); err != nil {
+
+	if err = AppCompose(viper, c.Gopath, c.Project, c.SubProjects); err != nil {
 		fmt.Printf("write to app error:%v", err)
 		return
 	}
-	_, err = Cmd("docker-compose -f app-compose.yml up")
-	fmt.Println(err)
+	if _, err = Cmd("docker-compose down"); err != nil {
+		fmt.Printf("err:%v", err)
+	}
+	fmt.Println("==> compose downed!")
+	if _, err = Cmd("docker-compose build"); err != nil {
+		fmt.Printf("err:%v", err)
+	}
+	fmt.Println("==> compose builded!")
+
+	if _, err = Cmd("docker-compose up"); err != nil {
+		fmt.Printf("err:%v", err)
+	}
+	fmt.Println("==> compose up!")
 
 }
 
@@ -68,35 +76,37 @@ func Cmd(cmd string) (out []byte, err error) {
 		out, err = exec.Command("bash", "-c", cmd).Output()
 	}
 	if err != nil {
-		panic("some error found")
+		panic(fmt.Sprintf("some error found:%v,detail:%v", err.Error(), string(out)))
 	}
 	return
 }
 
-func AppCompose(gopath string, project *ProjectDto, subProjects []*ProjectDto) error {
-	viper := viper.New()
+func AppCompose(viper *viper.Viper, gopath string, project *ProjectDto, subProjects []*ProjectDto) error {
 	appComposeMain(viper, gopath, project)
 	for _, project := range subProjects {
 		appCompose(viper, gopath, project)
 	}
-	viper.Set("networks.sharenet.external", true)
+	//viper.Set("networks.sharenet.driver", "bridge")
 	viper.Set("version", "3")
 	return viper.WriteConfig()
 }
 
 func appComposeMain(viper *viper.Viper, gopath string, project *ProjectDto) {
 	servicePre := "services." + project.Name
-	viper.SetConfigName("app-compose")
+
+	viper.SetConfigName("docker-compose")
 	viper.AddConfigPath(".")
 
+	project.SubNames = append(project.SubNames, "kafkaserver")
+	project.SubNames = append(project.SubNames, "mysqlserver")
 	viper.Set(servicePre+".build.context", gopath+"/src/"+project.Name)
 	viper.Set(servicePre+".build.dockerfile", "Dockerfile")
-	viper.Set(servicePre+".image", project.Name)
+	viper.Set(servicePre+".image", "test-"+project.Name)
+	viper.Set(servicePre+".restart", "on-failure:5")
 
-	viper.Set(servicePre+".container_name", project.Name)
-	// viper.Set(servicePre+".working_dir", ".")
-	// viper.Set(servicePre+".command", "go test")
-	viper.Set(servicePre+".networks", []string{"sharenet"})
+	viper.Set(servicePre+".container_name", "test-"+project.Name)
+	viper.Set(servicePre+".depends_on", project.SubNames)
+	//	viper.Set(servicePre+".networks", []string{"sharenet"})
 	viper.Set(servicePre+".ports", project.Ports)
 	viper.Set(servicePre+".environment", project.Envs)
 }
@@ -104,50 +114,38 @@ func appComposeMain(viper *viper.Viper, gopath string, project *ProjectDto) {
 //env format []string{"MYSQL_ROOT_PASSWORD=1234"}
 func appCompose(viper *viper.Viper, gopath string, project *ProjectDto) {
 	servicePre := "services." + project.Name
-	viper.SetConfigName("app-compose")
-	viper.AddConfigPath(".")
-
 	viper.Set(servicePre+".build.context", gopath+"/src/"+project.Name)
 	viper.Set(servicePre+".build.dockerfile", "Dockerfile")
-	viper.Set(servicePre+".image", project.Name)
+	viper.Set(servicePre+".image", "test-"+project.Name)
+	viper.Set(servicePre+".restart", "on-failure:5")
 
-	viper.Set(servicePre+".container_name", project.Name)
-	viper.Set(servicePre+".networks", []string{"sharenet"})
+	viper.Set(servicePre+".depends_on", []string{"mysqlserver"})
+	viper.Set(servicePre+".container_name", "test-"+project.Name)
+	//	viper.Set(servicePre+".networks", []string{"sharenet"})
 	viper.Set(servicePre+".ports", project.Ports)
 	viper.Set(servicePre+".environment", project.Envs)
 }
 
-func MysqlCompose(gopath string, envs []string) error {
-	viper := viper.New()
-	viper.SetConfigName("mysql-compose")
-	viper.AddConfigPath(".")
+func setComposeMysql(viper *viper.Viper, gopath string, envs []string) {
 	viper.Set("version", "3")
-	viper.Set("services.mysql.image", "gruppometasrl/mysql57")
-	viper.Set("services.mysql.container_name", "local-mysql")
-	viper.Set("services.mysql.restart", "always")
-	viper.Set("services.mysql.volumes", []string{gopath + ":/docker-entrypoint-initdb.d"})
-	viper.Set("services.mysql.ports", []string{"3306:3306"})
-	viper.Set("services.mysql.networks", []string{"sharenet"})
-	viper.Set("services.mysql.environment", envs)
-	viper.Set("networks.sharenet.external", true)
-	return viper.WriteConfig()
+	viper.Set("services.mysqlserver.image", "gruppometasrl/mysql57")
+	viper.Set("services.mysqlserver.container_name", "test-mysql")
+	viper.Set("services.mysqlserver.volumes", []string{gopath + ":/docker-entrypoint-initdb.d"})
+	viper.Set("services.mysqlserver.ports", []string{"3306:3306"})
+	viper.Set("services.mysqlserver.restart", "always")
+	//viper.Set("services.mysqlserver.networks", []string{"sharenet"})
+	viper.Set("services.mysqlserver.environment", envs)
 }
 
-func KafkaCompose() error {
-	viper := viper.New()
-	viper.SetConfigName("kafka-compose")
-	viper.AddConfigPath(".")
-	viper.Set("version", "3")
+func setComposeKafka(viper *viper.Viper) {
 	viper.Set("services.kafkaserver.image", "spotify/kafka:latest")
-	viper.Set("services.kafkaserver.container_name", "kafka")
+	viper.Set("services.kafkaserver.container_name", "test-kafka")
 	viper.Set("services.kafkaserver.hostname", "kafkaserver")
 	viper.Set("services.kafkaserver.restart", "always")
-	viper.Set("services.kafkaserver.networks", []string{"sharenet"})
+	//viper.Set("services.kafkaserver.networks", []string{"sharenet"})
 	viper.Set("services.kafkaserver.ports", []string{"2181:2181", "9092:9092"})
 	viper.Set("services.kafkaserver.environment", []string{"ADVERTISED_HOST=kafkaserver",
 		"ADVERTISED_PORT=9092"})
-	viper.Set("networks.sharenet.external", true)
-	return viper.WriteConfig()
 }
 
 func Read(env string, config interface{}) error {
